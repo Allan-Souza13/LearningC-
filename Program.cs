9e4c2ce32aa56a8050c98b735882b0a4ea273b63
Allan Pereira de Souza <77082266+Allan-Souza13@users.noreply.github.com>
2022-12-11 23:03:39 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,27 +1,20 @@
-﻿Double media = 0;
-Double idade1 = 0;
-Double idade2 = 0;
-Double idade3= 0;
-Double idade4 = 0;
-Double idade5 = 0;
+﻿int n = 0;
 
-Console.WriteLine("Programa que calcula a idade media de cinco alunos");
+int r = 0;
 
-Console.WriteLine("informe a idade do 1º Aluno : ");
-idade1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Determina se o número é par ou impar");
+Console.WriteLine("Informe um ´número: ");
 
-Console.WriteLine("Informe a idade do 2º Aluno : ");
-idade2 = Convert.ToDouble(Console.ReadLine());
+n = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Informe a idade do 3º Aluno : ");
-idade3 = Convert.ToDouble(Console.ReadLine());
+r = n % 2;
 
-Console.WriteLine("Informe a idade do 4º Aluno : ");
-idade4 = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Informe a idade do 5º Aluno : ");
-idade5 = Convert.ToDouble(Console.ReadLine());
-
-media = (idade1 + idade2 + idade3 + idade4 + idade5) / 5;
-Console.WriteLine("A ídade média dos Alunos é " + media);
+if (r == 1)
+{
+    Console.WriteLine("O nunero é impar");
+}
+else
+{
+    Console.WriteLine("O numero é Par");
+}
 Console.ReadKey();
